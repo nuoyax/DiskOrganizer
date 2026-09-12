0eae620dc8ead6a9dde7ba2f4aa2ac14e7dcd68f
halo <2862440280@qq.com>
2026-09-12 14:19:44 +0800

    ui: 默认窗口再缩为 720x500

--- src/app/ui/MainWindow.cpp
+++ src/app/ui/MainWindow.cpp
@@ -44,7 +44,7 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     refreshDisks();
     statusBar()->showMessage(tr("就绪"));
     setWindowTitle(tr("DiskOrganizer 磁盘整理助手"));
-    resize(880, 600);
+    resize(720, 500);
 }
 
 void MainWindow::buildUi() {
